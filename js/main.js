d7620cc1c921d8b0abdf57e1ce9f8121f63a6892
mykhatilo-vlad <mykhatilo.vlad@gmail.com>
2020-03-22 13:29:35 +0200

    new design site

--- js/main.js
+++ js/main.js
@@ -1,41 +1,101 @@
-$(document).ready(function() {
-  const portfolioSlider = $(".owl-carousel");
+(function ($) {
+	$('.comments__items').slick({
+		arrows: false,
+		autoplay: 5000,
+	});
 
-  portfolioSlider.on(
-    "initialize.owl.carousel initialized.owl.carousel " +
-      "initialize.owl.carousel initialize.owl.carousel" +
-      "resize.owl.carousel resized.owl.carousel " +
-      "refresh.owl.carousel refreshed.owl.carousel " +
-      "update.owl.carousel updated.owl.carousel " +
-      "drag.owl.carousel dragged.owl.carousel " +
-      "translate.owl.carousel translated.owl.carousel " +
-      "to.owl.carousel changed.owl.carousel",
-    function(event) {
-      $(".slide-controls-number__active").text(event.item.index + 1);
-      $(".slide-controls-number__total").text(event.item.count);
-    }
-  );
+	$('#commentsPrev').click(function () {
+		$('.comments__items').slick('slickPrev');
+	})
+	$('#commentsNext').click(function () {
+		$('.comments__items').slick('slickNext');
+	})
 
-  portfolioSlider.owlCarousel({
-    items: 1,
-    dots: false
-  });
+	var worksIn = anime({
+		targets: '.works-in',
+		keyframes: [
+			{
+				scale: 0.5,
+				rotate: '1turn'
+			},
+			
+		],
+		duration: 500,
+		easing: 'easeOutElastic(1, .8)',
+		autoplay: false,
+		direction: 'alternate'
+	});
 
-  portfolioSlider.on(
-    "drag.owl.carousel dragged.owl.carousel " +
-      "translate.owl.carousel translated.owl.carousel " +
-      "to.owl.carousel changed.owl.carousel",
-    function(event) {
-      $(".slide-controls-number__active").text(event.item.index + 1);
-      $(".slide-controls-number__total").text(event.item.count);
-    }
-  );
 
-  $("#portfolioArrowRight").click(function() {
-    portfolioSlider.trigger("next.owl.carousel");
-  });
+	$('.button-filter-all').on('click', function () {
+		setTimeout(
+			function () {
+				$('.works__item').css('display', 'block');
+			},
+			500);
 
-  $("#portfolioArrowLeft").click(function() {
-    portfolioSlider.trigger("prev.owl.carousel");
-  });
-});
+		$('.active').removeClass('active');
+		$(this).addClass('active');
+		worksIn.play();
+	});
+
+	$('.button-filter-html').on('click', function () {
+		setTimeout(
+			function () {
+				$('.filter-html').css('display', 'block');
+				$('.filter-wordpress').css('display', 'none');
+			},
+			500);
+
+		$('.active').removeClass('active');
+		$(this).addClass('active');
+		worksIn.play();
+	});
+
+	$('.button-filter-wordpress').on('click', function () {
+		setTimeout(
+			function () {
+				$('.filter-wordpress').css('display', 'block');
+				$('.filter-html').css('display', 'none');
+			},
+			500);
+
+		$('.active').removeClass('active');
+		$(this).addClass('active');
+		worksIn.play();
+	});
+
+	var mainMenu = $('.main-menu');
+	var clonedMenu = $(mainMenu.clone());
+	
+	$(document).scroll(function () {
+		if ($(document).width() > 785) {
+			$(clonedMenu).addClass('fixed');
+			$(mainMenu).before(clonedMenu);
+			if ($(document).scrollTop() > $('.main-menu').height() + 10) {
+				$(clonedMenu).css('top', '0');
+			} else {
+				$(clonedMenu).css('top', '-100%');
+			}
+		}
+	})
+
+	$(document).scroll(function () {
+		if ($(document).scrollTop() > $('.page-header').height() - 200) {
+			$('.page-footer__arrow').css('transform', 'scale(1)');
+		} else {
+			$('.page-footer__arrow').css('transform', 'scale(0)');
+		}
+	})
+
+	$('.page-header__burger').click(function () {
+		$('.page-header__burger, .page-header__nav').toggleClass('active');
+		$('body').toggleClass('lock');
+	})
+
+	$('.main-menu__item').click(function () {
+		$('.page-header__burger, .page-header__nav').removeClass('active');
+		$('body').removeClass('lock');
+	})
+
+})(jQuery);
